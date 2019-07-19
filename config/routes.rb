ba54c4e83b24578041a3e600f6ca1fe1ee33e711
Kentony <kenken.nyanko.1115@gmail.com>
2019-07-19 13:46:46 +0000

    login 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index' => "home#index"
+  root to: 'home#index'
   get 'home/:id/show' => "home#show"
   
   post 'home/:product_id/like' => "like#create"
   post 'home/:product_id/unlike' => "like#destroy"
   
-  #likeはhomeに移植したほうがいい？
-  
+
   get 'like' => "like#index"
   
 end
