e748cd0e4c2ac7ce044d7b58bcb9218297fd9291
Kentony <kenken.nyanko.1115@gmail.com>
2019-11-25 10:22:05 +0000

    seed priduct 9 まで終わり

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,38 +30,38 @@
   },
   {
     id: 5,
-    name: 'ATUSIDUN(アトストン）iPhone ケース リング 滑り防止 耐衝撃 iPhone XR ローズゴールド',
-    price: 1399,
-    star_count: 4.0,
-    url: 'https://images-na.ssl-images-amazon.com/images/I/61NbS3AZE6L._SX425_.jpg',
+    name: '防弾少年団 BTS BT21 iphoneケース シリコン電話ケースと携帯電話コードセット 二重の保護(i7/8, TATA)',
+    price: 1299,
+    star_count: 0,
+    url: 'https://www.amazon.co.jp/%E9%98%B2%E5%BC%BE%E5%B0%91%E5%B9%B4%E5%9B%A3-BT21-iphone%E3%82%B1%E3%83%BC%E3%82%B9-%E3%82%B7%E3%83%AA%E3%82%B3%E3%83%B3%E9%9B%BB%E8%A9%B1%E3%82%B1%E3%83%BC%E3%82%B9%E3%81%A8%E6%90%BA%E5%B8%AF%E9%9B%BB%E8%A9%B1%E3%82%B3%E3%83%BC%E3%83%89%E3%82%BB%E3%83%83%E3%83%88-%E4%BA%8C%E9%87%8D%E3%81%AE%E4%BF%9D%E8%AD%B7/dp/B07G58G4YJ',
   },
   {
     id: 6,
-    name: '【iPhone8/7/6s/6 ケース】IIII fit (ピンク)',
-    price: 2728,
-    star_count: 4.0,
-    url: 'https://unicase.jp/goods_image/003/750/37505_o.jpg',
+    name: 'iPhone XRケース シリコン かわいい ストラップホール 花柄 バラ 3D浮き彫り ディズニー キャラクター TPU 軽薄 衝撃吸収 女 携帯カバー iPhoneケース 6.1インチ (iPhone XR 6.1インチ, バラ1)',
+    price: 999,
+    star_count: 4.5,
+    url: 'https://www.amazon.co.jp/iPhone-%E3%82%B9%E3%83%88%E3%83%A9%E3%83%83%E3%83%97%E3%83%9B%E3%83%BC%E3%83%AB-3D%E6%B5%AE%E3%81%8D%E5%BD%AB%E3%82%8A-%E3%82%AD%E3%83%A3%E3%83%A9%E3%82%AF%E3%82%BF%E3%83%BC-iPhone%E3%82%B1%E3%83%BC%E3%82%B9/dp/B07J5PRTCL',
   },
   {
     id: 7,
-    name: 'i【ROOT CO.】iPhone XS Max 耐衝撃 iPhone ケース Gravity Shock Resist Case Pro. (イエロー/グロス)',
-    price: 4950,
-    star_count: 4.5,
-    url: 'https://images-na.ssl-images-amazon.com/images/I/61YVQCNIn8L._SY679_.jpg',
+    name: 'iPhone8 ケース iFace First Class ホワイト ガラスフィルム セット アイフォン8 カバー 耐衝撃 アイフォン ブランド アイフェイス iphoneケース simフリー スマホ カバー スマホケース スマートフォン',
+    price: 3900,
+    star_count: 0,
+    url: 'https://www.amazon.co.jp/iPhone8-%E3%82%AC%E3%83%A9%E3%82%B9%E3%83%95%E3%82%A3%E3%83%AB%E3%83%A0-%E3%82%A2%E3%82%A4%E3%83%95%E3%82%A9%E3%83%B38-iphone%E3%82%B1%E3%83%BC%E3%82%B9-%E3%82%B9%E3%83%9E%E3%83%BC%E3%83%88%E3%83%95%E3%82%A9%E3%83%B3/dp/B075MYYX7G',
   },
   {
     id: 8,
-    name: 'iFace Reflection iPhone8/7 ケース クリア 強化ガラス',
-    price: 4400,
-    star_count: 4.5,
-    url: 'https://images-na.ssl-images-amazon.com/images/I/61tS1AA8GrL._SY679_.jpg',
+    name: 'iPhone 6s ケース iphone 6 ケース 背面強化ガラスの携帯保護カバー 強化ガラスケース 全面保護カバー レンズ保護 ワイヤレスチャージャー対応 傷防止 手触りがよい 簡約風 創意 個性 スマホケース カバー 人気 かわいいSupreの写真 カップルのスタイル 高級感 iPhone 6S/6 対応 ケース (iPhone 6S/6, Supre-01)',
+    price: 1199,
+    star_count: 4.0,
+    url: 'https://www.amazon.co.jp/%E8%83%8C%E9%9D%A2%E5%BC%B7%E5%8C%96%E3%82%AC%E3%83%A9%E3%82%B9%E3%81%AE%E6%90%BA%E5%B8%AF%E4%BF%9D%E8%AD%B7%E3%82%AB%E3%83%90%E3%83%BC-%E5%BC%B7%E5%8C%96%E3%82%AC%E3%83%A9%E3%82%B9%E3%82%B1%E3%83%BC%E3%82%B9-%E3%83%AF%E3%82%A4%E3%83%A4%E3%83%AC%E3%82%B9%E3%83%81%E3%83%A3%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%BC%E5%AF%BE%E5%BF%9C-%E3%81%8B%E3%82%8F%E3%81%84%E3%81%84Supre%E3%81%AE%E5%86%99%E7%9C%9F-%E3%82%AB%E3%83%83%E3%83%97%E3%83%AB%E3%81%AE%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB/dp/B07P3HF5YT',
   },
   {
     id: 9,
-    name: '送料/関税込【Pornhub】 iPhone ケース',
-    price: 5300,
-    star_count: 0,
-    url: 'https://static-buyma-com.akamaized.net/imgdata/item/190125/0041083078/179925431/428.jpg'
+    name: 'Fuleadture iphone ケース シリコン 耐衝撃 薄型 脱着簡単 iPhone 7 Plus/8 Plus 用（ミントグリーン）',
+    price: 1499,
+    star_count: 4.0,
+    url: 'https://www.amazon.co.jp/Fuleadture-iphone-%E3%82%B1%E3%83%BC%E3%82%B9-%E3%82%B7%E3%83%AA%E3%82%B3%E3%83%B3-%E9%AB%98%E5%93%81%E8%B3%AA-%E8%80%90%E8%A1%9D%E6%92%83-%E8%96%84%E5%9E%8B-%E8%84%B1%E7%9D%80%E7%B0%A1%E5%8D%98-iPhone-%E7%94%A8%EF%BC%88%E3%83%9F%E3%83%B3%E3%83%88%E3%82%B0%E3%83%AA%E3%83%BC%E3%83%B3%EF%BC%89/dp/B0756CKXQX'
   },  
   {
     id: 10,
@@ -128,6 +128,113 @@ end
     url: 'https://images-na.ssl-images-amazon.com/images/I/61LFmakQArL._SY355_.jpg',
     sort_order: 2
   },
+  
+  
+  
+  
+  
+   {
+    id: 9,
+    product_id: 5,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/618Vk4eCYqL._AC_SL1410_.jpg',
+    sort_order: 1
+  },
+  {
+    id: 10,
+    product_id: 5,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61DUyWEkY1L._AC_SL1000_.jpg',
+    sort_order: 2
+  },
+   {
+    id: 11,
+    product_id: 5,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/6162TYeRpzL._AC_SL1000_.jpg',
+    sort_order: 3
+  },
+  
+  
+  {
+    id: 12,
+    product_id: 6,
+    url: 'hhttps://images-na.ssl-images-amazon.com/images/I/61y2aWU0TQL._AC_SL1000_.jpg',
+    sort_order: 1
+  },
+   {
+    id: 13,
+    product_id: 6,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/6192Eyfo57L._AC_SL1000_.jpg',
+    sort_order: 2
+  },
+  {
+    id: 14,
+    product_id: 7,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61UX4Sn2aEL._AC_SL1000_.jpg',
+    sort_order: 1
+  },
+   {
+    id: 15,
+    product_id: 7,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/51O-CxBmMGL._AC_.jpg',
+    sort_order: 2
+  },
+  {
+    id: 16,
+    product_id: 8,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/71Zgft4P7RL._AC_SL1500_.jpg',
+    sort_order: 1
+  },
+   {
+    id: 17,
+    product_id: 8,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/517vlY3z3cL._AC_SL1000_.jpg',
+    sort_order: 2
+  },
+  {
+    id: 18,
+    product_id: 9,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61GE3A8dtGL._AC_SL1001_.jpg',
+    sort_order: 1
+  },
+   {
+    id: 19,
+    product_id: 9,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/71pCIgDWf1L._AC_SL1001_.jpg',
+    sort_order: 2
+  },
+  {
+    id: 8,
+    product_id: 10,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61LFmakQArL._SY355_.jpg',
+    sort_order: 2
+  },
+   {
+    id: 20,
+    product_id: 10,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61LFmakQArL._SY355_.jpg',
+    sort_order: 1
+  },
+  {
+    id: 21,
+    product_id: 10,
+    url: 'https://images-na.ssl-images-amazon.com/images/I/61LFmakQArL._SY355_.jpg',
+    sort_order: 2
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 ].each do |product_image|
   unless ProductImage.find_by(id: product_image[:id])
     ProductImage.create(product_image)
@@ -222,7 +329,136 @@ iFaceって保証が付いてるから保証を使う事だって出来るはず
     star_count: 1.0,
     score: 67,
     title: '良い買い物でした'
+  },
+  
+  
+   {
+    id: 6,
+    product_id: 5,
+    review: ' ',
+    star_count: 0,
+    score:  0,
+    title: ' '
+  },
+   {
+    id: 7,
+    product_id: 5,
+    review: ' ',
+    star_count: 0,
+    score: 0,
+    title: ' '
+  },
+   {
+    id: 8,
+    product_id: 6,
+    review: 'ずっとiFaceを使っていたのですが大きいしただでさえ重い8plusがさらに重くなりかさばるので薄めの花柄がないかと探していたところ見つけました。柄も大好きな薔薇で気に入っています。衝撃には強そうに見えないけど薄くて持ちやすくていいです。',
+    star_count: 5.0,
+    score: 0,
+    title: '薄くて持ちやすい'
+  },
+   {
+    id: 9,
+    product_id: 7,
+    review: ' ',
+    star_count: 0,
+    score: 0,
+    title: ' '
+  },
+   {
+    id: 10,
+    product_id: 7,
+    review: ' ',
+    star_count: 0,
+    score: 0,
+    title: ' '
+  },
+ 
+   {
+    id: 12,
+    product_id: 8,
+    review: '見た目は違和感を全く感じず、
+ケース自体も薄いのでスリムに感じます。
+他の方がおっしゃってたように、
+背面がガラスというよりプラスチックっぽい印象です。
+探していた、薄くて傷がつきにくそうな商品だったので買ってよかったと思いました。',
+    star_count: 5.0,
+    score: 17,
+    title: 'とてもきれい'
+  },
+   {
+    id: 13,
+    product_id: 8,
+    review: '届いて１週間…軽く20センチほどの高さから落としたら…割れました…そしてガラスが手に突き刺さるし本当に強化ガラスなのかと言うくらいペラペラのガラスで強化ガラスで画面に貼るガラスよりペラペラです。
+
+デザインはいいのに残念です。
+
+皆さん怪我はしないように。',
+    star_count: 2.0,
+    score: 16,
+    title: 'デザインはいいのに。'
+  },
+   {
+    id: 14,
+    product_id: 9,
+    review: 'アップル純正シリコンケースのロゴなしバージョンと同クオリティのケース。高コスパで間違いなく使いやすくシンプルなケースです。ボタンだけは一体成型で非常に押しやすくなっている。ただ、シリコン特有の埃の吸着は否めない。程よいグリップ感で滑らなく滑らかで持っていてとても心地よい素材。パーフェクトなケースだと思うのでひとつ買っていてもよい商品。',
+    star_count: 5.0,
+    score: 6,
+    title: 'アップル純正シリコンのクオリティ'
+  },
+   {
+    id: 15,
+    product_id: 10,
+    review: '2個購入して、届いてすぐに確認したら背面のカメラレンズのすぐ下に傷がありました。
+
+1個は傷が薄く許容範囲だったのですがもう1個のキズが酷かったので交換可能かメールをしたのに返事がありません。(届いて確認後すぐにメール、返事が無いので4日後に再度メールしたけど未だ返事なし。)
+メールの返事が来るかもと思い、即箱に戻したので使うに使えない状態でどうしたら良いのか…
+
+他の通販サイトのレビューを見ても背面カメラレンズの下に傷があると書いてあるのも見かけたのでこれから購入される方は気を付けた方がいいと思います。
+
+背面なので開封しないと確認出来ないので困りますよね。
+気に入って購入しただけに非常に残念です。
+
+-追加-
+2度目のメールを送ってから1週間以上経ちましたが未だにメールの返事無し。
+iFaceって保証が付いてるから保証を使う事だって出来るはずなのに返事が無ければこちらはどうしようもありません。
+一体このキズ入りのiFaceどうしたら良いのでしょう？',
+    star_count: 1.0,
+    score: 67,
+    title: '良い買い物でした'
+  },
+   {
+    id: 16,
+    product_id: 10,
+    review: '2個購入して、届いてすぐに確認したら背面のカメラレンズのすぐ下に傷がありました。
+
+1個は傷が薄く許容範囲だったのですがもう1個のキズが酷かったので交換可能かメールをしたのに返事がありません。(届いて確認後すぐにメール、返事が無いので4日後に再度メールしたけど未だ返事なし。)
+メールの返事が来るかもと思い、即箱に戻したので使うに使えない状態でどうしたら良いのか…
+
+他の通販サイトのレビューを見ても背面カメラレンズの下に傷があると書いてあるのも見かけたのでこれから購入される方は気を付けた方がいいと思います。
+
+背面なので開封しないと確認出来ないので困りますよね。
+気に入って購入しただけに非常に残念です。
+
+-追加-
+2度目のメールを送ってから1週間以上経ちましたが未だにメールの返事無し。
+iFaceって保証が付いてるから保証を使う事だって出来るはずなのに返事が無ければこちらはどうしようもありません。
+一体このキズ入りのiFaceどうしたら良いのでしょう？',
+    star_count: 1.0,
+    score: 67,
+    title: '良い買い物でした'
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 ].each do |product_review|
   unless ProductReview.find_by(id: product_review[:id])
     ProductReview.create(product_review)
